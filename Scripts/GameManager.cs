31b538afbd908723edd09fe0938b818ec0e721c4
Sebe112 <92109053+Sebe112@users.noreply.github.com>
2025-04-04 21:39:36 +0200

    gamemanager booomba

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float speedAmplifier = 1.0f; // Speed multiplier for the player
+    public float healthAmplifier = 1.0f; // Health multiplier for the player
+    public float energyAmplifier = 1.0f; // Energy multiplier for the player
+    public float attackSpeedAmplifier = 1.0f; // Attack speed multiplier for the player
+    public float damageAmplifier = 1.0f; // Damage multiplier for the player
+    public int level = 1; // map level
+    public int courency = 0; // currency for the player
+    public bool isNightmare = false; // is the player in nightmare mode
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
