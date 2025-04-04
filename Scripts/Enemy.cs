b4fdf516027d04ef5f4d1402c6443acf06f83093
ayman <ayman.elm01@gmail.com>
2025-04-04 21:38:51 +0200

    scripts

---
+++ Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float heatlth;
+    public float damage;
+    public float speed;
+    public float spawnpropability;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
