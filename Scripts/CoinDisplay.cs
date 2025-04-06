47027e80f18d0b686f7f1bf415cb9aa19618c7f4
ayman <ayman.elm01@gmail.com>
2025-04-06 09:56:45 +0200

    penis

---
+++ Scripts/CoinDisplay.cs
@@ -0,0 +1,16 @@
+using TMPro;
+using UnityEngine;
+
+public class CoinDisplayTMP : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI coinTextTMP;
+
+    void Update()
+    {
+        if (GameManager.Instance != null)
+        {
+            // Update the TMP text with the coin amount
+            coinTextTMP.text = $"Coins: {GameManager.Instance.courency}";
+        }
+    }
+}
